8a773be16cfead47d7efa0cccc136fe71ef846a1
Sayvon <sedmon033@gmail.com>
2025-02-03 18:24:19 -0600

    add like controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const clothingItemRouter = require("./clothingItems");
 const userRouter = require("./users");
+const likeRouter = require("./likes");
 const NotFoundError = require("../utils/errorclasses/NotFoundError");
 
 router.use("/clothing-items", clothingItemRouter);
+router.use("/clothing-items", likeRouter);
 router.use("/users", userRouter);
 
 // Defensive code
-router.use(() => {
-  throw new NotFoundError("The Requested resource was not found.");
-});
+// router.use(() => {
+//   throw new NotFoundError("The Requested resource was not found.");
+// });
 
 module.exports = router;
