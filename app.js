d8782ef20f140788c4f8d3caa96d4c671ee4558c
Sayvon <sedmon033@gmail.com>
2025-03-14 21:44:45 -0500

    cors issue

--- app.js
+++ app.js
@@ -26,13 +26,11 @@ startApp(); // Could be fixed later
 
 
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://travelwear-aa3b8a7cc158.herokuapp.com']
-}));
+app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
-app.use("/uploads", cors({ origin: "http://localhost:3000" }), express.static(path.join(__dirname, 'public', 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, 'public', 'uploads')));
 app.use("/packing-lists", packingLists);
 
 
