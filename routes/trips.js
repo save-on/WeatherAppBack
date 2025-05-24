d1f9d91e8c1a21310829daf8aadd4db460e8a687
MrSurge87 <Mendozsa.sm@gmail.com>
2025-05-24 12:42:58 -0400

    added trips route for saved trips

---
+++ routes/trips.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middlewares/auth");
+const tripsController = require("../controllers/trips");
+
+
+router.post("/", auth, tripsController, createTripWithPackingList);
+
+module.export = router;
