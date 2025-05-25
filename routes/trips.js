3cfa5289238f9d739da6972c870665f06fe4ce3c
MrSurge87 <Mendozsa.sm@gmail.com>
2025-05-25 03:21:17 -0400

    added get trip by ID to trip controller and route

--- routes/trips.js
+++ routes/trips.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
-const { createTripWithPackingList, getTrips } = require("../controllers/trips");
+const { createTripWithPackingList, getTrips, getTripById } = require("../controllers/trips");
 
 router.post("/", auth, createTripWithPackingList);
 
 router.get("/", auth, getTrips);
+router.get("/:tripId", auth, getTripById);
 
 module.exports = router;
